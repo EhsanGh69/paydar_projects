62b71001cb67a4b4fb8f14987ea329a5ea6b69ad
EhsanGh <ehsan.gh69@gmail.com>
2023-05-03 21:34:48 +0330

    making-persian-admin

--- account/admin.py
+++ account/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
+
+admin.site.site_header = "پروژه‌های پایدار"
+admin.site.site_title = "مدیریت سایت"
+admin.site.index_title = "پروژه‌های پایدار"
+
 admin.site.register(User, UserAdmin)
