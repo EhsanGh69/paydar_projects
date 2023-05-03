22b66974b46a100bf870f2fa2c99cf000a8df8ef
EhsanGh <ehsan.gh69@gmail.com>
2023-05-03 22:55:06 +0330

    unregister-Group-model

--- account/admin.py
+++ account/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 from .models import User
 
 
@@ -8,3 +9,4 @@ admin.site.site_title = "مدیریت سایت"
 admin.site.index_title = "پروژه‌های پایدار"
 
 admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
