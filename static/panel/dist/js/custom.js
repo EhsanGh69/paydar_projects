8dbbe4a41e9e63de1722c95794d56ac9e3f54469
EhsanGh <ehsan.gh69@gmail.com>
2023-06-05 21:21:22 +0330

    add-sortable-custom-printThis-files

---
+++ static/panel/dist/js/custom.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $('#print-btn').click(function (e) { 
+        e.preventDefault();
+
+        $('.action-th').remove();
+        $('.action-td').remove();
+
+        $('#data-table').printThis();
+
+        setTimeout(function(){
+            location.reload();
+        }, 1000);
+        
+    });
+});
+
