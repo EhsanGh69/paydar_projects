85e173c756e719822ee8f994b3a46550b8400286
EhsanGh <ehsan.gh69@gmail.com>
2023-06-15 13:00:19 +0330

    add-confirm-delete-file

---
+++ static/panel/dist/js/confirm_delete.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let form_confirm = document.querySelector('#form_confirm_modal')
+    let buttons = document.querySelectorAll("[data-target='#deleteItemModal']");
+        buttons.forEach(button => {
+            button.addEventListener("click", () => {
+
+                 // extract texts from calling element and replace the modals texts with it
+                if (button.dataset.message) {
+                    document.getElementById("modal-message").innerHTML = button.dataset.message;
+                }
+                // extract url from calling element and replace the modals texts with it
+                if (button.dataset.url) {
+                    form_confirm.action = button.dataset.url;
+                }
+
+            })
+        });
+   let confirmModal = document.getElementById("confirmButtonModal")
+    confirmModal.addEventListener('click', () => {
+        form_confirm.submit();
+    });
+});
